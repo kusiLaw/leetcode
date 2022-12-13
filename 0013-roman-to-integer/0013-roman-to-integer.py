22da6200e46e3fa705eac7492aa809fd074cde1f
Lawrence Kusi Addai <lawcubegsm@gmail.com>
2022-12-13 09:49:28 +0000

    Time: 75 ms (72.07%), Space: 13.9 MB (30.97%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,9 +2,9 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         sum = 0
-
-        for index in range(len(s)):
-            if index + 1 <  len(s) and roman[s[index]] < roman[s[index + 1]]:
+        l = len(s)
+        for index in range(l):
+            if index + 1 <  l and roman[s[index]] < roman[s[index + 1]]:
                     sum -= roman[s[index]]
             else:
                 sum += roman[s[index]]
