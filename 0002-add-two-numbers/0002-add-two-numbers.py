b31887f7afbfaa25c13d22d4f04ae27553288dbf
Lawrence Kusi Addai <lawcubegsm@gmail.com>
2022-12-26 12:48:42 +0000

    Time: 80 ms (74.68%), Space: 14 MB (43.33%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,29 +5,33 @@
 #         self.next = next
 
 def reverse_node_list(node):
-    # check firt node 
-    if node.next is None: # if next is empty retur the val, the default is zero
-        return node.val
-    
-    head = [node.val]  # keep firts node val in list
-    next_node = node.next  # store next node
-    
-    while True:     # keep loop 
-        if next_node.next != None :
-            head.insert(0, next_node.val)  # since it need the reverse order as result
-            next_node = next_node.next  # ref next_node for next iteration
-        else:
-            head.insert(0, next_node.val) # since no next item grap the val
-            break
-    return ''.join([str(x) for x in head]) # return result str
-    
+         # check firt node 
+        if node.next is None: # if next is empty retur the val, the default is zero
+            return node.val
+
+        head = [node.val]  # keep firts node val in list
+        next_node = node.next  # store next node
+
+        while True:     # keep loop 
+            if next_node.next != None :
+                head.insert(0, next_node.val)  # since it need the reverse order as result
+                next_node = next_node.next  # ref next_node for next iteration
+            else:
+                head.insert(0, next_node.val) # since no next item grap the val
+                break
+        return ''.join([str(x) for x in head]) # return result str
+
+
     
 def insertNode(node, tail_node = None): # take two nod an return the tail node
-    node.next = tail_node
-    return  tail_node
+        node.next = tail_node
+        return  tail_node
+
+    
     
 
 class Solution:
+    
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
             # retrieve each node val in inverse order as string
             # convert to itn and add them
@@ -35,19 +39,19 @@ class Solution:
             
             
             # create first node from the result
-            
-            head_node = ListNode( int(str(ans)[-1]) )
+            ans = list(str(ans)[::-1])  # convert ans to str; reverse it;
+            head_node = ListNode( ans[0]) 
             
             tail_node = head_node  # head node is also seen as last node
             
-            for x in list(str(ans)[::-1])[1:]: #convert ans to str; reverse it; loop from index 1
+            for x in ans[1:]: # loop from index 1
                 # each iteration ref new created node, since the new created node become tail node
-                tail_node = insertNode(tail_node, ListNode(int(x))) 
+                tail_node = insertNode(tail_node, ListNode(x)) 
             
 
             return head_node   # return instance of a node
             
 
-                    
-            
+
+
         
