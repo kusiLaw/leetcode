2d0a5c49f9a5e26f0f5e1738e5e556308635f289
Lawrence Kusi Addai <lawcubegsm@gmail.com>
2023-03-07 10:07:30 +0000

    Time: 4529 ms (15.96%), Space: 50.2 MB (93.30%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -9,7 +9,8 @@ var containsNearbyDuplicate = function(nums, k) {
     for(let i = 0; i < nums.length; i +=1 ){
         
         for(let j =  i + 1; j < nums.length; j +=1){
-              if(nums[i] == nums[j] && Math.abs(i-j) <= k){
+              if(nums[i] == nums[j] && (j-i) <= k){
+                  
                   return true
               }
         }   
