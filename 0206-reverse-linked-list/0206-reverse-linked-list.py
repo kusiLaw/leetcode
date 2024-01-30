8cbecb62133d4683513da9322efad6f0f1ec2bd8
Lawrence Kusi Addai <lawcubegsm@gmail.com>
2024-01-30 07:36:25 +0000

    Time: 42 ms (43.00%), Space: 17.8 MB (88.30%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,31 +5,53 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        if not head or head.next == None:
-            return head
-        
-        tail = head # head become  tail
-        current_head = head # assum as tail at step 1
+        #optimise using two pointer approach
         
         
-        current_node = head.next
-        while current_node != None:
-            # let tail point to the current node next item
-            tail.next = current_node.next
+        # let begin from None and point every next node to previous and
+        # return previous as new had when we meet None again
             
-            #let the current node point to current head
-            current_node.next = current_head 
+        prev, current = None, head
+        
+        while current: # reach none and stop loop
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+        return prev
             
-            #let the current node be the head        
-            current_head  = current_node 
+            
+        
+        
+################################### working ########################################
+# 44 ms, faster than 32.50% of Python3 online submissions for Reverse Linked List. #
+# 17.7 MB, less than 96.12% of Python3 online submissions for Reverse Linked List. #
+####################################################################################
+
+#         if not head or head.next == None:
+#             return head
+        
+#         tail = head # head become  tail
+#         current_head = head # assum as tail at step 1
+        
+        
+#         current_node = head.next
+#         while current_node != None:
+#             # let tail point to the current node next item
+#             tail.next = current_node.next
+            
+#             #let the current node point to current head
+#             current_node.next = current_head 
+            
+#             #let the current node be the head        
+#             current_head  = current_node 
                 
-            if tail.next == None:
-                # print(current_node,'\n', '---', tail.next)
-                return current_head
-            else:
+#             if tail.next == None:
+#                 # print(current_node,'\n', '---', tail.next)
+#                 return current_head
+#             else:
                
-                current_node = tail.next #where the current nod were suppose to point at
+#                 current_node = tail.next #where the current nod were suppose to point at
  
-        return  current_head
+#         return  current_head
     
